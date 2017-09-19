4f4e6b19f95c83d8da2d3e0b8c0345a4561befbb
zhangqi <zhangqi_248@qq.com>
2017-09-19 10:26:34 +0800

    增加aparbet转换

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import subprocess
 import uuid
 from webapp import app, mongo
 from flask import jsonify
+from arpabetandipaconvertor.ipa2arpabet import IPA2ARPAbetConvertor
 
 @app.route('/crawl/<string:word>')
 def crawl_world(word):
@@ -16,16 +17,22 @@ def crawl_world(word):
 
     wait for  this command to finish, and read output.json to client.
     """
-    alphabet = None
+    alphabet_item = None
     spider_names = ["iciba", "youdao"]
     for spider_name in spider_names:
         subprocess.check_output(['scrapy', 'crawl', spider_name, "-a", "word=" + word])
-        alphabet = mongo.find_one('dict', {"word": word})
-        if alphabet:
+        alphabet_item = mongo.find_one('dict', {"word": word})
+        if alphabet_item:
             break
 
-    if alphabet:
-        return jsonify({"result": 0, "data": {"word": word, "arpabet": alphabet["american_phonetic_alphabet"]}})
+    if alphabet_item:
+
+        if alphabet_item:
+            alphabet = alphabet_item["american_phonetic_alphabet"]
+            if alphabet:
+                convert = IPA2ARPAbetConvertor()
+                arpabet = convert.convert(alphabet)
+                return jsonify({"result": 0, "data": {"word": word, "arpabet": arpabet}})
 
     return jsonify({"result": 1000, "data": "查不到"})
 
